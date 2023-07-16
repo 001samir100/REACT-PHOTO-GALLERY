d2ed7f611b775621d3de79a4af8b8647b9651944
Samir Shrestha <pit.sam.010@gmail.com>
2023-07-16 22:17:15 +0545

    image skeletons and others are added in 1st branch

---
+++ src/components/skeleton/ImageSkeleton.jsx
@@ -0,0 +1,12 @@
+import { BiSolidImage } from "react-icons/bi";
+const ImageSkeleton = () => {
+	return (
+		<div className=" rounded-md w-[30%] h-[30%] ">
+			<div className="animate-pulse w-full h-full">
+				<BiSolidImage className="text-light_orange w-full h-full" />
+			</div>
+		</div>
+	);
+};
+
+export default ImageSkeleton;
